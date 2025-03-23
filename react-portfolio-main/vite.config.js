8e8d6ac363407297bedb3be420eef6256fb212f4
subhan513 <subhancs57@gmail.com>
2025-03-24 00:58:31 +0500

    done

--- react-portfolio-main/vite.config.js
+++ react-portfolio-main/vite.config.js
@@ -4,10 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // This ensures correct routing on Vercel
-  css: {
-    modules: {
-      localsConvention: "camelCase",
-    },
+  base: "/", // Ensure base path is correct
+  build: {
+    outDir: "dist",
   },
 });
