d96d4ed6430fa3d5961adbb1a7322c1ed7197f77
subhan513 <subhancs57@gmail.com>
2025-03-24 01:09:26 +0500

    done

--- react-portfolio-main/vite.config.js
+++ react-portfolio-main/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/", // Ensure base path is correct
+  base: "/react-portfolio", // Ensure base path is correct
   build: {
     outDir: "dist",
   },
